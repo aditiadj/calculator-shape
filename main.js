f1c7697277917c283eecace9487d7d0ff377e422
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-16 15:27:51 +0700

    Complete level 1 calc shape

---
+++ main.js
@@ -0,0 +1,52 @@
+let luasPersegi = document.querySelector(`#squareArea`)
+let kelilingPersegi = document.querySelector(`#squarePerimeter`)
+let luasLingkaran = document.querySelector(`#circleArea`)
+let kelilingLingkaran = document.querySelector(`#circleCircum`)
+let luasKubus = document.querySelector(`#cubeArea`)
+let volumeKubus = document.querySelector('#cubeVolume')
+
+function squareArea(length) {
+  length = Number(document.getElementById(`input-one`).value)
+  result = Math.pow(length, 2);
+  document.getElementById(`result`).innerHTML = result
+}
+luasPersegi.addEventListener('click', squareArea)
+
+
+function squarePerimeter(length) {
+  length = Number(document.getElementById(`input-one`).value)
+  result = 4 * length
+  document.getElementById(`result`).innerHTML = result
+}
+kelilingPersegi.addEventListener('click', squarePerimeter)
+
+
+function circleArea(radius) {
+  r = Number(document.getElementById(`input-one`).value)
+  result = Math.PI * Math.pow(r, 2)
+  document.getElementById(`result`).innerHTML = result
+}
+luasLingkaran.addEventListener('click', circleArea)
+
+
+function circleCircum(radius) {
+  r = Number(document.getElementById(`input-one`).value)
+  radius = 2 * Math.PI * r
+  document.getElementById(`result`).innerHTML = radius
+}
+kelilingLingkaran.addEventListener('click', circleCircum)
+
+
+function cubeArea(length) {
+  length = Number(document.getElementById(`input-one`).value)
+  result = 6 * Math.pow(length, 2);
+  document.getElementById(`result`).innerHTML = result
+}
+luasKubus.addEventListener('click', cubeArea)
+
+function cubeVolume(length) {
+  length = Number(document.getElementById(`input-one`).value)
+  result = Math.pow(length, 3);
+  document.getElementById(`result`).innerHTML = result
+}
+volumeKubus.addEventListener('click', cubeVolume)
